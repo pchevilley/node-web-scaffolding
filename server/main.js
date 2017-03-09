7f9c5014b9252235c4dc2deee790db614d26cd44
Thomas Lanteigne <boggan@tlanteigne-iMac.local>
2017-03-09 13:03:02 -0500

    - added sample greetings API entry point /api/greetings/<your name>

--- server/main.js
+++ server/main.js
@@ -18,12 +18,52 @@ function cApplication() {
 
     //=============================================================================
     this.handleAPIRequest = function(i_oAPIInfos, i_oCallback) {
-        i_oCallback("API Request Handled! " + JSON.stringify(i_oAPIInfos));
+        var l_sRootRestSection = i_oAPIInfos.restChunks[0];
+
+        switch (l_sRootRestSection) {
+            case "section1":
+                _handleAPISection1(i_oAPIInfos, i_oCallback);
+                break;
+            case "section2":
+                _handleAPISection2(i_oAPIInfos, i_oCallback);
+                break;
+            case "greetings":
+                _handleGreetings(i_oAPIInfos, i_oCallback);
+                break;
+            default:
+                i_oCallback("Unknown API " + JSON.stringify(i_oAPIInfos));
+        }
     };
 
     //=============================================================================
     // Private Methods
     //=============================================================================
+
+    //=============================================================================
+    function _handleAPISection1(i_oAPIInfos, i_oCallback) {
+        i_oCallback("You have reached API Section1");
+    }
+
+    //=============================================================================
+    function _handleAPISection2(i_oAPIInfos, i_oCallback) {
+        i_oCallback("You have reached API Section2");
+    }
+
+    //=============================================================================
+    function _handleGreetings(i_oAPIInfos, i_oCallback) {
+        var l_aPossibleGreetings = ["¡Hola", "Hallo", "Hi", "Bonjour", "Привіт", "Hej", "Oy", "ハイ", "สวัสดี"],
+            l_sName = decodeURIComponent(i_oAPIInfos.restChunks[1]) || "John Doe",
+            l_nRandomIndex = Math.round(Math.random() * (l_aPossibleGreetings.length - 1)),
+            l_sGreeting;
+
+        l_sName = l_sName
+            .split(/\s+/)
+            .map(i_sChunk => i_sChunk[0].toUpperCase() + i_sChunk.slice(1))
+            .join(' ');
+
+        l_sGreeting = l_aPossibleGreetings[l_nRandomIndex] + " " + l_sName;
+        i_oCallback(l_sGreeting);
+    }
 
     //=============================================================================
     function _constructor_() {
